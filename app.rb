da4142694bf1ee3794544e1b74cba9e7c03d0a5d
Nicholas Schaefer <nick@nicholasschaefer.com>
2024-07-01 13:00:10 -0400

    completed 3, Adding an index page

--- app.rb
+++ app.rb
@@ -1,8 +1,31 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require "tilt/erubis"
+require 'pry'
+
+before do
+  ROOT = File.expand_path("..", __FILE__)
+end
+
+
+helpers do
+  def base_name(full_path)
+    File.basename(full_path)
+  end
+end
 
 get '/' do
-  # erb "Getting Started"
-  erb :test
+  paths = Dir["#{ROOT}/data/*"]
+  @files = paths.select { |path| File.file?(path)}
+
+  erb :index
 end
+
+# File.file?(paths[-1])
+
+
+# Dir["./data/*"]
+# => ["./data/about.txt", "./data/changes.txt", "./data/history.txt"]
+
+# Dir.entries("./data")
+# => ["..", "about.txt", "history.txt", "changes.txt", "."]
