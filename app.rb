909eff830b57bbba19c687a98a5d618c74383653
Nicholas Schaefer <nick@nicholasschaefer.com>
2024-07-04 12:14:04 -0400

    completed 6, Handling Requests for Nonexistent Documents

--- app.rb
+++ app.rb
@@ -1,28 +1,54 @@
 require 'sinatra'
-require 'sinatra/reloader'
-require "tilt/erubis"
-# require 'pry'
+require 'sinatra/reloader' if development?
+# require "sinatra/content_for"
 
-before do
-  ROOT = File.expand_path("..", __FILE__)
+require "tilt/erubis"
+require 'pry'
+
+configure do
+  enable :sessions
+  set :session_secret, SecureRandom.hex(32)
 end
 
 
+before do
+  ROOT = File.expand_path("..", __FILE__)
+  session[:flash_messages]  ||= []
+  session[:previous_path] ||= ""
+end
+
 helpers do
+  def h(text)
+    Rack::Utils.escape_html(text)
+  end
+
+  def hattr(text)
+    Rack::Utils.escape_path(text)
+  end
+
   def base_name(full_path)
     File.basename(full_path)
   end
 end
 
-get '/' do
+def homepage()
+  @error = session[:flash_messages].delete_at(0)
+  # session[:previous_path] = "muffin"
+
   paths = Dir["#{ROOT}/data/*"]
   @files = paths.select { |path| File.file?(path)}
 
-  erb :index
+  body erb :index
+end
+
+get '/' do
+  # status_code = session[:flash_messages].empty? ? 200 : 404
+  homepage()
 end
 
 get '/:file' do
-  absolute_path = ROOT + "/data/" + params[:file]
+  @path = params[:file]
+  absolute_path = ROOT + "/data/" + @path
   # "<p>this route works!</p>"
 
   if File.exist?(absolute_path)
@@ -31,7 +57,18 @@ get '/:file' do
     body \
       File.read(absolute_path)
   else
-    erb :page_not_found
+    session[:previous_path] = @path
+    session[:flash_messages][0] = "#{@path} does not exist."
+
+    status 404
+    redirect "/"
+    # status    404
+    # body      homepage()
+
+    # headers
+    # body      erb :page_not_found
+    # body      erb :index
+
   end
 end
 
